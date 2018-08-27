6778a847ff0d0b4527b3a82a5819e9f488c52f07
tikTokTik <mithoonk@flock.com>
2018-08-27 12:25:19 +0530

    Use of reflection

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,47 +1,100 @@
 package com.company;
-import java.util.HashMap;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.reflect.Constructor;
 
-class custom {
-    public int num;
-    public String str;
-    public custom(int num, String str){
-        this.num = num;
-        this.str = str;
+class Test
+{
+    // creating a private field
+    private String s;
+
+    // creating a public constructor
+    public Test()  {  s = "GeeksforGeeks"; }
+
+    // Creating a public method with no arguments
+    public void method1()  {
+        System.out.println("The string is " + s);
     }
 
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == this) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        custom c = (custom) obj;
-        if (c.num == this.num && this.str.equals(c.str)) {
-            return true;
-        }
-        return false;
+    // Creating a public method with int as argument
+    public void method2(int n)  {
+        System.out.println("The number is " + n);
     }
 
-    @Override
-    public int hashCode() {
-        return 1;
+    // creating a private method
+    private void method3() {
+        System.out.println("Private method invoked");
     }
 }
 
-public class Main {
+public class Main
+{
+    public static void main(String args[]) throws Exception
+    {
+        // Creating object whose property is to be checked
+        Test obj = new Test();
 
-    public static void main(String args[]) {
-        HashMap<String, custom> map = new HashMap<>();
-        custom c1 = new custom(1,"mithoon");
-        custom c2 = new custom(2,"mithoon");
-        map.put("first", c1);
-        map.put("first", c2);
-        System.out.println(c1.equals(c2));
-        System.out.println("checked");
+        // Creating class object from the object using
+        // getclass method
+        Class cls = obj.getClass();
+        System.out.println("The name of class is " +
+                cls.getName());
+        System.out.println(obj);
+
+        // Getting the constructor of the class through the
+        // object of the class
+        Constructor constructor = cls.getConstructor();
+        System.out.println("The name of constructor is " +
+                constructor.getName());
+
+        System.out.println("The public methods of class are : ");
+
+        // Getting methods of the class through the object
+        // of the class by using getMethods
+        Method[] methods = cls.getMethods();
+
+        // Printing method names
+        for (Method method:methods)
+            System.out.println(method.getName());
+
+        // creates object of desired method by providing the
+        // method name and parameter class as arguments to
+        // the getDeclaredMethod
+        Method methodcall1 = cls.getDeclaredMethod("method2",
+                int.class);
+        // invokes the method at runtime
+        methodcall1.invoke(obj, 19);
+
+        // creates object of the desired field by providing
+        // the name of field as argument to the 
+        // getDeclaredField method
+        Field field = cls.getDeclaredField("s");
+
+        // allows the object to access the field irrespective
+        // of the access specifier used with the field
+        field.setAccessible(true);
+
+        // takes object and the new value to be assigned
+        // to the field as arguments
+        field.set(obj, "JAVA");
+
+        // Creates object of desired method by providing the
+        // method name as argument to the getDeclaredMethod
+        Method methodcall2 = cls.getDeclaredMethod("method1");
+
+        // invokes the method at runtime
+        methodcall2.invoke(obj);
+
+        // Creates object of the desired method by providing
+        // the name of method as argument to the 
+        // getDeclaredMethod method
+        Method methodcall3 = cls.getDeclaredMethod("method3");
+
+        // allows the object to access the method irrespective 
+        // of the access specifier used with the method
+        methodcall3.setAccessible(true);
+
+        // invokes the method at runtime
+        methodcall3.invoke(obj);
     }
 }
-
-
